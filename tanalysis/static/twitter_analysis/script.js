646009959314f41725669b9661a566b9d640fec0
hiroki <nz-programming>
2022-08-21 13:37:48 -0400

    add information about sentiment score ranking

--- tanalysis/static/twitter_analysis/script.js
+++ tanalysis/static/twitter_analysis/script.js
@@ -4,8 +4,8 @@ const data_average_polality = dataset["average_polality"]
 const data_polality_json = dataset["polality_json"]
 const data_keyword = dataset["keyword_count"]
 
-// const data = JSON.parse('{{data_json|safe}}')
 
+// chart with chart.js
 var context = document.getElementById("myChart");
 var myLineChart  = new Chart(context, {
 type: 'line',
@@ -41,12 +41,12 @@ options: {
 
 
 function clickHandler(click){
-// console.log(click)
 const points = myLineChart.getElementsAtEventForMode(click, 'nearest', {intersect: true}, true);
-// console.log(points)
 if(points[0]){
     // console.log(points[0].datasetIndex)
     // console.log(points[0].index)
+
+    // set index of cliked item on html
     const dataset = points[0].datasetIndex;
     const index = points[0].index;
     console.log(index)
@@ -61,29 +61,53 @@ if(points[0]){
     // // console.log(myLineChart.data.labels[index])
     const sample =  document.querySelector('.sample');
     const sample_2 =  document.querySelector('.sample_2');
-    const sample_3 =  document.querySelector('.sample_3');
+
+    // grab html tag for user icon for sentiment score ranking
+    const sent_icon_1 =  document.querySelector('.sent_icon_1');
+    const sent_icon_2 =  document.querySelector('.sent_icon_2');
+    const sent_icon_3 =  document.querySelector('.sent_icon_3');
+
+    // grab html tag for sentiment score for sentiment score ranking
+    const sent_score_1 =  document.querySelector('.sent_score_1');
+    const sent_score_2 =  document.querySelector('.sent_score_2');
+    const sent_score_3 =  document.querySelector('.sent_score_3');
+
+    // grab html tag for text for sentiment score ranking
+    const sent_text_1 =  document.querySelector('.sent_text_1');
+    const sent_text_2 =  document.querySelector('.sent_text_2');
+    const sent_text_3 =  document.querySelector('.sent_text_3');
+
     sample.innerText = label;
     sample_2.innerText = keyword;
 
-    const sentimentscore = polality_json["sentiment_score"]
-    console.log(sentimentscore)
+    const sentiment_score = polality_json["sentiment_score"]
 
-
-    let arr = Object.keys(sentimentscore).map((e)=>({ key: e, value: sentimentscore[e] }));
-    console.log(arr);
+    // sort object accoring to sentiment score
+    let arr = Object.keys(sentiment_score).map((e)=>({ key: e, value: sentiment_score[e] }));
     arr.sort(function(a,b){
         if(a.value < b.value) return 1;
         if(a.value > b.value) return -1;
         return 0;
     });
-    console.log(arr);
-    let key_arr = Object.keys(arr);
-    console.log(key_arr);
+
+    // user icon for sentiment score ranking
+    sent_icon_1.setAttribute('src', polality_json["profile_img"][arr[0].key]);
+    sent_icon_2.setAttribute('src', polality_json["profile_img"][arr[1].key]);
+    sent_icon_3.setAttribute('src', polality_json["profile_img"][arr[2].key]);
+
+    // sentiment score for sentiment score ranking
+    sent_score_1.innerText = polality_json["sentiment_score"][arr[0].key];
+    sent_score_2.innerText = polality_json["sentiment_score"][arr[1].key];
+    sent_score_3.innerText = polality_json["sentiment_score"][arr[2].key];
+
+    // text for sentiment score ranking
+    sent_text_1.innerText = polality_json["text"][arr[0].key];
+    sent_text_2.innerText = polality_json["text"][arr[1].key];
+    sent_text_3.innerText = polality_json["text"][arr[2].key];
 
 
-    sample_3.setAttribute('src', polality_json["profile_img"]["1"]);
-    // sample_3.src = polality_json["profile_img"]["0"];
-    console.log(sample_3);
+
+
 }
 }
 myLineChart.canvas.onclick = clickHandler;
